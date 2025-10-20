740ab2ce3c28a05f5d5ddabb37cca58b1255377b
1iseo <96890370+1iseo@users.noreply.github.com>
2025-10-20 16:57:26 +0700

    fix vite entry point

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,12 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>CAKAP FT MAOS - Sistem Monitoring APAR</title>
-    
+
     <link rel="icon" href="/favicon.ico" type="image/x-icon">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 
     @viteReactRefresh
-    @vite(['resources/css/app.css', 'resources/css/vendor-fixes.css', 'resources/css/exception-fixes.css', 'resources/js/app.jsx'])
+    @vite(['resources/css/app.css', 'resources/css/vendor-fixes.css', 'resources/css/exception-fixes.css', 'resources/js/app.tsx'])
 
     <style>
         body {
